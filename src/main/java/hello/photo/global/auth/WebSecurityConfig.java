3c523bf1a800b0d68b0766ac9a45bdb1f9ca3de6
so1eeee <soljeong821@gmail.com>
2024-07-30 20:51:44 +0900

    change config

--- src/main/java/hello/photo/global/auth/WebSecurityConfig.java
+++ src/main/java/hello/photo/global/auth/WebSecurityConfig.java
@@ -18,22 +18,22 @@ public class WebSecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                //csrf 보안 비활성화
-                .csrf(csrf -> csrf.disable())
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                //stateless 상태로 관리하기에 csrf 보안 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
                 //폼 기반 인증 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)
                 //HTTP Basic 인증 비활성화
-                .httpBasic(httpBasic -> httpBasic.disable())
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        //일단은 모두 /** 처리 -> 추후에 수정 예정
+                        //일단은 모두 허용 가능하게 처리 -> 추후에 수정 예정
                         .requestMatchers("/api/signup", "/api/login", "/**", "/error","/swagger-ui/**", "/v3/api0-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                //세션 사용 X
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //세션을 Stateless 상태로 관
+                .sessionManagement((session) ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //JWT 인증이 먼저 처리되고 나머지 필터들이 JWT 필터에 의해 설정된 인증 정보를 사용할 수 있게 함
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
