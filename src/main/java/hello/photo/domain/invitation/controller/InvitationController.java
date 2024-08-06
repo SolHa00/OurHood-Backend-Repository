3f7d06e73d50f7aab7491eb269b2faa0d94ae7fa
so1eeee <soljeong821@gmail.com>
2024-08-06 20:05:55 +0900

    초대 요청 처리 API

--- src/main/java/hello/photo/domain/invitation/controller/InvitationController.java
+++ src/main/java/hello/photo/domain/invitation/controller/InvitationController.java
@@ -3,25 +3,29 @@ package hello.photo.domain.invitation.controller;
 
 import hello.photo.domain.invitation.dto.request.InvitationRequest;
 import hello.photo.domain.invitation.service.InvitationService;
+import hello.photo.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/invitations")
 @Tag(name = "Invitation API", description = "초대 요청 관련 API")
 public class InvitationController {
 
     private final InvitationService invitationService;
 
-    @PostMapping("/invitations")
+    @PostMapping
     @Operation(summary = "방 초대 요청 API")
     public void createInvitation(@RequestBody InvitationRequest request) {
         invitationService.createInvitation(request.getRoomId(), request.getNickname());
+    }
+
+    @DeleteMapping("/{invitationId}")
+    @Operation(summary = "방 초대 요청 처리 API")
+    public ApiResponse handleInvitation(@PathVariable Long invitationId, String action) {
+        return invitationService.handleInviteRequest(invitationId, action);
     }
 }
