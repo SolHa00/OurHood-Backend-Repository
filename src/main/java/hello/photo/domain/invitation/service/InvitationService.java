c2974d7586c08c7e233d744b84e63a2e21073bf1
so1eeee <soljeong821@gmail.com>
2024-08-06 20:05:55 +0900

    초대 요청 처리 API

--- src/main/java/hello/photo/domain/invitation/service/InvitationService.java
+++ src/main/java/hello/photo/domain/invitation/service/InvitationService.java
@@ -4,7 +4,10 @@ import hello.photo.domain.invitation.entity.Invitation;
 import hello.photo.domain.invitation.repository.InvitationRepository;
 import hello.photo.domain.room.entity.Room;
 import hello.photo.domain.room.repository.RoomRepository;
+import hello.photo.domain.user.entity.User;
 import hello.photo.domain.user.repository.UserRepository;
+import hello.photo.global.response.ApiResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +22,28 @@ public class InvitationService {
     public void createInvitation(Long roomId, String nickname) {
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new RuntimeException("해당 Room 존재하지 않음"));
+        User user = userRepository.findByNickname(nickname)
+                .orElseThrow(() -> new RuntimeException("해당 유저가 존재하지 않음"));
 
         Invitation invitation = new Invitation();
         invitation.setRoom(room);
+        invitation.setUser(user);
 
         invitationRepository.save(invitation);
     }
-}
 
+
+    @Transactional
+    public ApiResponse handleInviteRequest(Long invitationId, String action) {
+        Invitation invitation = invitationRepository.findById(invitationId)
+                .orElseThrow(() -> new RuntimeException("해당 초대 요청을 찾을 수 없음"));
+        if("accept".equalsIgnoreCase(action)) {
+            Room room = invitation.getRoom();
+            User user = invitation.getUser();
+            room.getMembers().add(user);
+            roomRepository.save(room);
+        }
+        invitationRepository.delete(invitation);
+        return new ApiResponse("초대 요청을 " + action + " 했습니다.");
+    }
+}
