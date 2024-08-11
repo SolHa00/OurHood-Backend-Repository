f458e63daf5d3291b5e61746af61030adc8f1836
so1eeee <soljeong821@gmail.com>
2024-08-12 03:49:58 +0900

    remove anno

--- src/main/java/hello/photo/domain/moment/controller/MomentController.java
+++ src/main/java/hello/photo/domain/moment/controller/MomentController.java
@@ -1,13 +1,11 @@
 package hello.photo.domain.moment.controller;
 
-import hello.photo.domain.moment.dto.MomentRequest;
 import hello.photo.domain.moment.service.MomentService;
 import hello.photo.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
