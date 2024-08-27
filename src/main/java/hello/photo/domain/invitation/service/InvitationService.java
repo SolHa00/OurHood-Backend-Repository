44dafa49754c31f095e6a899e0a98298c921aa1b
so1eeee <soljeong821@gmail.com>
2024-08-27 22:00:01 +0900

    ?

--- src/main/java/hello/photo/domain/invitation/service/InvitationService.java
+++ src/main/java/hello/photo/domain/invitation/service/InvitationService.java
@@ -1,9 +1,8 @@
 package hello.photo.domain.invitation.service;
 
 import hello.photo.domain.invitation.entity.Invitation;
-import hello.photo.domain.room.entity.Room;
-import hello.photo.domain.user.entity.User;
 import hello.photo.domain.invitation.repository.InvitationRepository;
+import hello.photo.domain.room.entity.Room;
 import hello.photo.domain.room.repository.RoomRepository;
 import hello.photo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +19,9 @@ public class InvitationService {
     public void createInvitation(Long roomId, String nickname) {
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new RuntimeException("해당 Room 존재하지 않음"));
-        User user = userRepository.findByNickname(nickname)
-                .orElseThrow(() -> new RuntimeException("해당 닉네임을 가진 User 존재하지 않음"));
 
         Invitation invitation = new Invitation();
         invitation.setRoom(room);
-        invitation.setUser(user);
 
         invitationRepository.save(invitation);
     }
