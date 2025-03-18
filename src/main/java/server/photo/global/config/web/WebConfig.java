d9782aed986d2243fcd7e871a523ae5ae17fba18
so1eeee <soljeong821@gmail.com>
2025-03-19 00:52:36 +0900

    fix : cors 에러

--- src/main/java/server/photo/global/config/web/WebConfig.java
+++ src/main/java/server/photo/global/config/web/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ourhood-two.vercel.app")
+                .allowedOrigins("https://www.ourhood01.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
