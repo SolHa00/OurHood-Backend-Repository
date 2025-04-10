efa3d4dfea0e5be54e9cca2b7cf8da2f9974a219
so1eeee <soljeong821@gmail.com>
2025-04-10 21:40:36 +0900

    fix: deploy.sh에서 로그 디렉토리 자동 생성 로직 추가

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,13 @@
          sleep 3
  fi
 
+LOG_DIR="/home/ubuntu/ourhood/ourhood-prod-logs"
+if [ ! -d "$LOG_DIR" ]; then
+    echo "Creating log directory at $LOG_DIR"
+    mkdir -p "$LOG_DIR"
+fi
+
  JAR_PATH="/home/ubuntu/ourhood/photo-0.0.1-SNAPSHOT.jar"
  echo "jar path : $JAR_PATH"
  chmod +x $JAR_PATH
  nohup java -jar $JAR_PATH --spring.profiles.active=prod > /home/ubuntu/ourhood/log.out 2>&1 &
-
