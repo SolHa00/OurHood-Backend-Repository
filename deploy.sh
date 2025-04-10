fa08ec850012aef7eb2efe30d46329e2cb59b744
so1eeee <soljeong821@gmail.com>
2025-04-10 21:52:16 +0900

    fix: logback 설정 반영 및 로그 파일 생성되지 않는 문제 해결

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
- CURRENT_PID=$(pgrep -f .jar)
- echo "$CURRENT_PID"
- if [ -z "$CURRENT_PID" ]; then
-         echo "no process"
- else
-         echo "kill $CURRENT_PID"
-         kill -9 "$CURRENT_PID"
-         sleep 3
- fi
+CURRENT_PID=$(pgrep -f .jar)
+echo "$CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+        echo "no process"
+else
+        echo "kill $CURRENT_PID"
+        kill -9 "$CURRENT_PID"
+        sleep 3
+fi
 
 LOG_DIR="/home/ubuntu/ourhood/ourhood-prod-logs"
 if [ ! -d "$LOG_DIR" ]; then
@@ -15,7 +15,7 @@ if [ ! -d "$LOG_DIR" ]; then
     mkdir -p "$LOG_DIR"
 fi
 
- JAR_PATH="/home/ubuntu/ourhood/photo-0.0.1-SNAPSHOT.jar"
- echo "jar path : $JAR_PATH"
- chmod +x $JAR_PATH
- nohup java -jar $JAR_PATH --spring.profiles.active=prod > /home/ubuntu/ourhood/log.out 2>&1 &
+JAR_PATH="/home/ubuntu/ourhood/photo-0.0.1-SNAPSHOT.jar"
+echo "jar path : $JAR_PATH"
+chmod +x $JAR_PATH
+nohup java -jar $JAR_PATH --spring.profiles.active=prod &
